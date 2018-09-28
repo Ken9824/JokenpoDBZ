8745ba23a550dff0aeb4ba2110fd1be9055c69ce
Logon Aluno <logonrmlocal@fiap.com.br>
2018-09-28 09:46:15 -0300

    Initial commit

---
+++ app/src/main/java/com/example/logonrmlocal/jokenballz/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.logonrmlocal.jokenballz
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        btJogar.setOnClickListener {
+            val intent = Intent(this@MainActivity, GameActivity::class.java)
+            startActivity(intent)
+        }
+
+    }
+
+
+
+}
